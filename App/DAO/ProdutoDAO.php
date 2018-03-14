e931362ab150cf6bc6347d8d72b54187c76754c6
Celso Henrique Vieira Pinto <celsovipsp@hotmail.com>
2018-03-13 22:22:47 -0400

    130318

--- App/DAO/ProdutoDAO.php
+++ App/DAO/ProdutoDAO.php
@@ -9,7 +9,85 @@
 namespace App\DAO;
 
 
-class ProdutoDAO
+class ProdutoDAO extends Conexao
+
 {
+    public function inserir ($produto)
+    {
+        $sql = "insert into produtos (id, descricao, quantidade, valor, marca, validade)VALUES (:id, :descricao, :quantidade, :valor, :marca, :validade)";
+        try{
+            $in = $this->conexao->prepare($sql);
+            $in->bindValue("id",$produto->getId());
+            $in->bindValue("descricao",$produto->getDescricao());
+            $in->bindValue("quantidade",$produto->getQauntidade());
+            $in->bindValue("valor",$produto->getValor());
+            $in->bindValue("marca",$produto->getMarca());
+            $in->bindValue("validade",$produto->getValidade());
+            $in->execute();
+            return true;
+        } catch (\PDOException $exception){
+            echo "<div class='alert alert-danger'>{$exception->getMessage()}</div>";
+        }
+    }
+
+    public function pesquisar($produto=null)
+    {
+     $sql = "select * from produtos where descricao like :descricao";
+     try {
+         $pes = $this->conexao->prepare($sql);
+         $pes->bindValue(":descricao","%". $produto->getDescricao()."%");
+         $pes->execute();
+         return $pes->fetchAll(\PDO::FETCH_CLASS,"\App\Model\Produto");
+
+     } catch (\PDOException $exception){
+         echo  "<div class = 'alert alert-danger'>";
+         $exception->getMessage();
+     }
+    }
+
+    public  function excluir ($produto)
+    {
+        $sql = "delete from produtos where id = :id";
+        try {
+            $i = $this->conexao->prepare($sql);
+            $i->bindValue(":id", $produto->getId());
+            $i->execute();
+            return true;
+        } catch (\PDOException $exception) {
+            echo "<div class='alert alert-danger'>{$exception->getMessage()}</div>";
+        }
+    }
+
+    public function pesquisarUM($produto)
+    {
+        $sql = "Select * from  produtos where id = :id" ;
+        try {
+            $p = $this->conexao->prepare($sql);
+            $p->bindValue("id", $produto->getId());
+            $p->execute();
+            return $p->fetch(\PDO::FETCH_ASSOC);
+
+        }   catch (\PDOException $exception){
+            echo "<div class='alert alert-danger'>{$exception->getMessage()}</div>";
+        }
+    }
+
+    public function alterar($produto)
+    {
+        $sql = "update produtos set descricao = :descricao, quantidade = :quantidade, valor = :valor, validade = :validade where id = :id";
+        try {
+            $i = $this->conexao->prepare($sql);
+            $i->bindValue(":id",$produto->getId());
+            $i->bindValue(":descricao",$produto->getDescicao());
+            $i->bindValue(":quantidade",$produto->getQuantidade());
+            $i->bindValue(":valor",$produto->getValor());
+            $i->bindValue(":validade",$produto->getValidade());
+            $i->execute();
+            return true;
+        }   catch (\PDOException $e) {
+            echo "<div class='alert alert-danger'>{$e->getMessage()}</div>";
+        }
+    }
+
 
 }
